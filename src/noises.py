4d783b5b0c685434346b69dd27df0432431724c0
alexisjapas <alexis.japas.dev@pm.me>
2022-11-02 22:21:20 +0100

    added comments

--- src/noises.py
+++ src/noises.py
@@ -3,16 +3,21 @@ import numpy as np
 
 
 def salt_and_pepper(image, density):
+    # Transforms and compute image data
     image = image.astype(float)
     max_image = np.max(image)
     min_image = np.min(image)
+
+    # Browse all the pixels of the image
     for y in range(image.shape[0]):
         for x in range(image.shape[1]):
+            # For each, replaces its value with either its min or max
             if density * 100 > float(uniform(0, 100)):
                 if randint(0, 1):
                     image[y, x] = max_image
                 else:
                     image[y, x] = min_image
+
     return image
 
 
